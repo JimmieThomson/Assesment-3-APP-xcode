05ca3a0a0ef1b8e67fffe34edba5ae221b6c037a
James Thomson <101159512+JimmieThomson@users.noreply.github.com>
2022-05-29 14:00:17 +1000

    Xcode

---
+++ IOS Assesment 3 application/IOS_Assesment_3_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Assesment_3_applicationApp.swift
+//  IOS Assesment 3 application
+//
+//  Created by James Thomson on 26/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Assesment_3_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
